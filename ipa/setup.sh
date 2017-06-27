66c677a3de7848443ed59fac09f7c8d2705337d9
Christian Heimes <christian@python.org>
2017-06-27 18:17:06 +0200

    Deploy FreeIPA 4.5.2 on Fedora

--- ipa/setup.sh
+++ ipa/setup.sh
@@ -5,7 +5,7 @@ vagrant up --no-provision
 vagrant provision
 
 echo
-echo "Rrun 'bin/ipa_kinit admin' to acquire a Kerberos ticket"
+echo "Run 'bin/ipa_kinit admin' to acquire a Kerberos ticket"
 echo "The admin password is 'Secret123'"
 echo "The DM password is 'DMSecret456'"
 echo
